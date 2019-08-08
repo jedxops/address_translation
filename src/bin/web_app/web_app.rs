c3a99f6099c462e34861fd2f22080ffad636ae63
Jeff <jja6@pdx.edu>
2019-08-07 19:48:32 -0700

    src/bin files

---
+++ src/bin/web_app/web_app.rs
@@ -0,0 +1,42 @@
+// Copyright © 2019 Jeff Austin, Kamakshi Nagar
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+// rocket necessities
+#![feature(proc_macro_hygiene, decl_macro)]
+
+// rocket necessities
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+use rocket_contrib::templates::Template;
+#[macro_use]
+extern crate serde_derive;
+
+// modules for other files (functions we need in those files)
+mod calculations;
+mod lib_fns;
+mod routes;
+
+// launcher
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().attach(Template::fairing()).mount(
+        "/",
+        routes![
+            routes::file,
+            routes::index,
+            routes::q_format_0,
+            routes::q_format_1,
+            routes::q_format_2,
+            routes::q_format_3,
+            routes::setup,
+            routes::solution,
+        ],
+    )
+}
+
+// main driver program
+fn main() {
+    rocket().launch();
+}
